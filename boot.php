3584de2158138d75445ec0e97e95139be84365a3
Rémi <remi@cazalet.net>
2013-10-09 17:37:39 +0200

    Path correction (adding a missing slash)

--- boot.php
+++ boot.php
@@ -4,7 +4,7 @@ define( 'CITRUS_PATH', dirname( __FILE__ ) );
 define( 'CITRUS_CLASS_PATH', CITRUS_PATH . '/core/' );
 define( 'CITRUS_APPS_PATH', CITRUS_PATH . '/apps/' );
 define( 'CITRUS_LIB_PATH', CITRUS_CLASS_PATH . 'lib/' );
-define( 'CITRUS_WWW_PATH', CITRUS_PATH . 'www/' );
+define( 'CITRUS_WWW_PATH', CITRUS_PATH . '/www/' );
 define( 'CITRUS_LOG_PATH', CITRUS_PATH . '/log/' );
 define( 'CITRUS_RW_EXT', '.html' );
 
