c952df9063df8d08f106abe22bdfedabe987eba1
Rémi <remi@cazalet.net>
2013-10-17 18:57:39 +0200

    Typos correction

--- apps/install/modules/main/templates/model.tpl.php
+++ apps/install/modules/main/templates/model.tpl.php
@@ -3,19 +3,19 @@
 	Création des schémas sql à partir des entités crées et référencées par Citrus.
 </p>
 <form action="buildSchema.html" id="shbuild">
-	<button>Générer les shémas SQL</button>
+	<button>Générer les schémas SQL</button>
 </form>
 <hr class="cb" />
 <p>
 	Insertion du schéma sql dans la base de donnée de l'host utilisé
 </p>
 <form action="execSchema.html" id="shexec">
-	<button <?php if (!$shema) echo 'disabled="disabled"'; ?>>Injecter directement dans la base de donnée</button>
+	<button <?php if (!$shema) echo 'disabled="disabled"'; ?>>Injecter directement dans la base de données</button>
 </form>
 <hr class="cb" />
 <p>
 	Export du schéma sql
 </p>
 <form action="dlSchema.html" id="shdl">
-	<button <?php if (!$shema) echo 'disabled="disabled"'; ?>>Télécharger les shémas</button>
+	<button <?php if (!$shema) echo 'disabled="disabled"'; ?>>Télécharger les schémas</button>
 </form>
