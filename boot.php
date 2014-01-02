29b7cff4df31a36e690c94713047a22d3788e047
Rémi <remi@caramia.fr>
2014-01-02 16:21:26 +0100

    Adding constant CITRUS_SCHEME to detect https

--- boot.php
+++ boot.php
@@ -8,18 +8,19 @@ define( 'CITRUS_WWW_PATH', CITRUS_PATH . '/www/' );
 define( 'CITRUS_LOG_PATH', CITRUS_PATH . '/log/' );
 define( 'CITRUS_RW_EXT', '.html' );
 
+define( 'CITRUS_SCHEME', isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] == 'on' ? 'https://' : 'http://' );
+
 include_once CITRUS_PATH . '/include/functions.php';
 include_once CITRUS_CLASS_PATH . '/Citrus/Citrus.php';
 
 session_start();
-
 require CITRUS_CLASS_PATH . 'doctrine-orm/Doctrine/ORM/Tools/Setup.php';
 
 $lib = CITRUS_CLASS_PATH . "doctrine-orm/";
 Doctrine\ORM\Tools\Setup::registerAutoloadDirectory( $lib );
 
 try {
-    $cos = core\Citrus\Citrus::getInstance( !isset( $_SERVER['HTTP_HOST'] ) );    
+    $cos = core\Citrus\Citrus::getInstance( !isset( $_SERVER['HTTP_HOST'] ) );
 } catch ( core\Citrus\sys\Exception $e ) {
     core\Citrus\sys\Debug::handleException( $e, true );
 }
