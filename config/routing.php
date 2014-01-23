c1884d3c1b07a4f5a85669da243223bbaf8351be
Rémi <remi@caramia.fr>
2014-01-23 13:45:38 +0100

    cleans routing.php, no default route added except for statics

--- config/routing.php
+++ config/routing.php
@@ -1,18 +1,9 @@
 <?php
 
 return array(
-    'default' => array(
-        'app' => 'install',
-        'controller' => 'main',
-        'action' => 'index',
-    ),
     'routes' => array( 
-		array(
-			'url'    => '/', 
-        	'target' => array( 'controller' => 'main' ) 
-		),
         array(
-            'url'    => '/static/:app/:type/:file:ext', 
+            'url'    => '/:app/:type/:file:ext', 
             'target' => array( 
                 'action' => 'static',
             ),
@@ -21,7 +12,7 @@ return array(
                 'file' => ".*",
                 'ext'  => "\.[A-Za-z0-9]+"
             ),
-        )
+        ),
     ),
     
 );
